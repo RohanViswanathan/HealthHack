e830da0658ca38b637406ddbf6dcc3e661977cd6
Thomas Liang <thomas.liang@warriorlife.net>
2019-01-20 10:54:59 -0800

    Added table and reminders

---
+++ MARx/MARx/Reminders.swift
@@ -0,0 +1,20 @@
+//
+//  Reminders.swift
+//  MARx
+//
+//  Created by Thomas Liang on 1/20/19.
+//  Copyright © 2019 Solstice. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Reminders: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
