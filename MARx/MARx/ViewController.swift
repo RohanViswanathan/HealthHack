5a9f8c401eeccc5f932c405b189f4009c66a40f8
Thomas Liang <thomas.liang@warriorlife.net>
2019-01-20 13:54:59 -0800

    add med

--- MARx/MARx/ViewController.swift
+++ MARx/MARx/ViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var popup: UIView!
+    @IBOutlet weak var medName: UITextField!
+    @IBOutlet weak var medID: UITextField!
+    @IBOutlet weak var medInfo: UITextView!
+    @IBOutlet weak var days: UITextField!
+    @IBOutlet weak var times: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
     }
 
+    @IBAction func addMeds(_ sender: Any) {
+        popup.isHidden = false
+    }
+    
+    @IBAction func SaveMed(_ sender: Any) {
+        let name = medName.text
+        let id = medID.text
+        let info = medInfo.text
+        let days = medName.text!
+        let times = medName.text!
+        
+        let fullString = name+";"+id+";"+info+";"+days+";"+times
+        
+        let meds = UserDefaults.standard.array(forKey: "meds")
+        meds.append(fullString)
+        
+        UserDefaults.standard.set(meds, forKey: "meds")
+        
+        popup.isHidden = true
+    }
 }
 
