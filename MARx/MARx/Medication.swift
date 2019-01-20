c27b1206af105232806987ff5e4e0822dcf05c26
Linus Zheng <linus.zheng@warriorlife.net>
2019-01-20 11:08:48 -0800

    added medication and reminder class

---
+++ MARx/MARx/Medication.swift
@@ -0,0 +1,22 @@
+//
+//  Medication.swift
+//  MARx
+//
+//  Created by Linus Zheng on 1/20/19.
+//  Copyright © 2019 Solstice. All rights reserved.
+//
+
+import Foundation
+
+class Medication: Reminder {
+    
+    var imprint: String
+    var dosage: String
+    
+    init(imprint: String, dosage: String, howOften: String) {
+        self.imprint = imprint
+        self.dosage = dosage
+        super.init(howOften: howOften)
+    }
+    
+}
