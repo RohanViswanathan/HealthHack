26941dd76567236fb07391c0cb5e90abe698ab9f
Rohan Viswanathan <rohanviswanathan@Rohans-MacBook-Pro.local>
2019-01-20 10:27:04 -0800

    Created XCode Project

---
+++ MARx/MARx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MARx
+//
+//  Created by Rohan Viswanathan on 1/20/19.
+//  Copyright © 2019 Solstice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
