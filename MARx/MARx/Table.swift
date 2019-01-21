22451a047fca63df225aec42260f16dab446cc56
Rohan Viswanathan <rohanviswanathan@Rohans-MacBook-Pro.local>
2019-01-20 17:09:26 -0800

    Added Reminders

--- MARx/MARx/Table.swift
+++ MARx/MARx/Table.swift
@@ -22,6 +22,11 @@ class Table {
         w = width
         h = height
         
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        
         if (UserDefaults.standard.array(forKey: "meds") != nil) {
             if (UserDefaults.standard.array(forKey: "meds")!.count > 0) {
                 let meds = UserDefaults.standard.array(forKey: "meds") as! [String]
@@ -47,15 +52,30 @@ class Table {
                     box.layer.borderColor = UIColor.lightGray.cgColor
                     box.addTarget(view, action: #selector(buttonPressed), for: .touchUpInside)
                     
+                    let takeTimes = parts[parts.count-1].components(separatedBy: ",")
+                    var nextTime = ""
+                    if (takeTimes.count > 0) {
+                        for n in 0...takeTimes.count-1 {
+                            let num = (takeTimes.count-1)-n
+                            let timeParts = takeTimes[num].components(separatedBy: ":")
+                            if (Int(timeParts[0])! > hour) {
+                                nextTime = takeTimes[num]
+                            } else if (Int(timeParts[0])! == hour) {
+                                if (Int(timeParts[1])! > minute) {
+                                    nextTime = takeTimes[num]
+                                }
+                            }
+                        }
+                    }
                     
-                    let time = UILabel(frame: CGRect(x: 250, y: 50+(i*100), width: width-248, height: 100))
-                    time.text = "12:00"
-                    time.textColor = UIColor.lightGray
+                    let timeLabel = UILabel(frame: CGRect(x: 250, y: 50+(i*100), width: width-248, height: 100))
+                    timeLabel.text = nextTime
+                    timeLabel.textColor = UIColor.lightGray
                     
                     scroll.addSubview(box)
-                    scroll.addSubview(time)
+                    scroll.addSubview(timeLabel)
                     boxes.append(box)
-                    times.append(time)
+                    times.append(timeLabel)
                 }
             }
         }
@@ -76,6 +96,11 @@ class Table {
         let numBoxes = meds.count
         print(numBoxes)
         
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        
         for i in 0...numBoxes-1 {
             let string = meds[i]
             let parts = string.components(separatedBy: ";")
@@ -91,9 +116,25 @@ class Table {
             box.layer.borderColor = UIColor.lightGray.cgColor
             box.addTarget(view, action: #selector(buttonPressed), for: .touchUpInside)
             
+            let takeTimes = parts[parts.count-1].components(separatedBy: ",")
+            
+            var nextTime = ""
+            if (takeTimes.count > 0) {
+                for n in 0...takeTimes.count-1 {
+                    let num = (takeTimes.count-1)-n
+                    let timeParts = takeTimes[num].components(separatedBy: ":")
+                    if (Int(timeParts[0])! > hour) {
+                        nextTime = takeTimes[num]
+                    } else if (Int(timeParts[0])! > hour) {
+                        if (Int(timeParts[1])! > minute) {
+                            nextTime = takeTimes[num]
+                        }
+                    }
+                }
+            }
             
             let time = UILabel(frame: CGRect(x: 250, y: 50+(i*100), width: w-248, height: 100))
-            time.text = "12:00"
+            time.text = nextTime
             time.textColor = UIColor.lightGray
             
             scroll.addSubview(box)
